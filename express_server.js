5d0288726b000a70c7e61a07340d41599a75f84a
Wares <fazelyar.wares@gmail.com>
2020-04-13 23:22:27 +0000

    added urls_new.ejs and corresponding get route. displays an input form

--- express_server.js
+++ express_server.js
@@ -28,10 +28,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-})
+});
+
 
 // listens to start server
 app.listen(PORT, () => {
