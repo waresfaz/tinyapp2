557d1ed591875ac8f82e0e208f9a6483d72c11bc
Wares <fazelyar.wares@gmail.com>
2020-04-13 23:25:45 +0000

    installed body-parser middleware and added corresponding require code to express_server.js

--- express_server.js
+++ express_server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
