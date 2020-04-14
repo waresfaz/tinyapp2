bbf5af0e87483ce82f31e5af6f20e41ef95bb320
Wares <fazelyar.wares@gmail.com>
2020-04-14 19:46:06 +0000

    added delete route into express_server.js

--- express_server.js
+++ express_server.js
@@ -67,6 +67,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  // console.log(urlDatabase[req.params.shortURL]);
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls");
+});
 
 // listens to start server
 app.listen(PORT, () => {
